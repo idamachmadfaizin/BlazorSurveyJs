9f683edbea1e73a55689e86ec20c00607967a18b
Idam <idamachmadfaizin@gmail.com>
2023-10-22 13:18:19 +0700

    survey form ok using knockout

--- BlazorSurveyJs/Pages/SurveyForm.razor.js
+++ BlazorSurveyJs/Pages/SurveyForm.razor.js
@@ -1,1 +1,17 @@
-//# sourceMappingURL=SurveyForm.razor.js.map
+export function init() {
+    const surveyJson = {
+        elements: [{
+                name: "FirstName",
+                title: "Enter your first name:",
+                type: "text"
+            }, {
+                name: "LastName",
+                title: "Enter your last name:",
+                type: "text"
+            }]
+    };
+    const survey = new Survey.Model(surveyJson);
+    ko.applyBindings({
+        model: survey
+    });
+}
---
+++ BlazorSurveyJs/Pages/SurveyForm.razor.ts
@@ -0,0 +1,22 @@
+declare const Survey;
+declare const ko;
+
+export function init() {
+    const surveyJson = {
+        elements: [{
+            name: "FirstName",
+            title: "Enter your first name:",
+            type: "text"
+        }, {
+            name: "LastName",
+            title: "Enter your last name:",
+            type: "text"
+        }]
+    };
+
+    const survey = new Survey.Model(surveyJson);
+
+    ko.applyBindings({
+        model: survey
+    });
+}
